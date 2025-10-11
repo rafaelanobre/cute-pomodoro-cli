bbba554a5923c0448c5a5f828f25993a18c8dc01
Rafaela Nobre <raafanobre@gmail.com>
2025-10-11 17:39:36 -0300

    Feat: add customizable timer and cycles args

--- pomodoro_timer/config.py
+++ pomodoro_timer/config.py
@@ -30,8 +30,40 @@ class PomodoroConfig:
             choices=["pink", "blue", "default"],
             help="Choose a color for the timer display"
         )
+        parser.add_argument(
+            "--work",
+            type=int,
+            default=self.work_mins,
+            metavar="MINUTES",
+            help="Duration of work sessions in minutes (default: 25)"
+        )
+        parser.add_argument(
+            "--short-break",
+            type=int,
+            default=self.short_break_mins,
+            metavar="MINUTES",
+            help="Duration of short breaks in minutes (default: 5)"
+        )
+        parser.add_argument(
+            "--long-break",
+            type=int,
+            default=self.long_break_mins,
+            metavar="MINUTES",
+            help="Duration of long break in minutes (default: 15)"
+        )
+        parser.add_argument(
+            "--cycles",
+            type=int,
+            default=self.number_of_cycles,
+            metavar="NUMBER",
+            help="Number of work/break cycles before long break (default: 4)"
+        )
 
         args = parser.parse_args()
 
         self.theme = args.theme
         self.color = args.color
+        self.work_mins = args.work
+        self.short_break_mins = args.short_break
+        self.long_break_mins = args.long_break
+        self.number_of_cycles = args.cycles
